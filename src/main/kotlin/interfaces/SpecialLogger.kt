d15c34af7a9b4b2859df00c616d3bda2c1d4d9d1
Roy <roy@osherove.com>
2022-07-18 02:31:35 +0300

    Versions and initial files

---
+++ src/main/kotlin/interfaces/SpecialLogger.kt
@@ -0,0 +1,5 @@
+package calculations.interfaces
+
+interface SpecialLogger {
+    fun write(text:String)
+}
---
+++ src/test/kotlin/initial/CalcTests.kt
@@ -0,0 +1,13 @@
+package initial
+
+import calculations.interfaces.SpecialLogger
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+class CalcTests {
+   @Test
+   fun `some test`(){
+
+   }
+
+}
