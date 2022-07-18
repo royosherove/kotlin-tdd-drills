e6af1a43086ba86f74abe39515f614b420f3d2ce
Roy <roy@osherove.com>
2022-07-18 10:00:45 +0300

    Simple Calculator

---
+++ src/main/kotlin/SimpleCalculator.kt
@@ -0,0 +1,9 @@
+
+class SimpleCalculator () {
+     fun add(x:Int,y:Int): Int {
+         if (x<0 ){
+             return 0
+         }
+         return x+y
+    }
+}
--- src/test/kotlin/initial/CalcTests.kt
+++ src/test/kotlin/initial/CalcTests.kt
@@ -1,5 +1,6 @@
 package initial
 
+import StringCalcWithLogger
 import calculations.interfaces.SpecialLogger
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
